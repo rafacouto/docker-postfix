e919e34b17d14e9da41aa5f1e962e1eff031b87d
Rafa Couto <caligari@treboada.net>
2016-12-29 09:38:22 +0100

    Essential files when running chrooted.

--- assets/postfix.sh
+++ assets/postfix.sh
@@ -5,9 +5,8 @@ postconf -e mydomain="$DOMAIN"
 postconf -e smtpd_banner="\$myhostname ESMTP"
 postconf -e mail_spool_directory="/var/spool/mail/"
 postconf -e mydestination="localhost, $myhostname, $mydomain"
-postconf -Me submission/inet="submission inet n - - - - smtpd"
 
 postfix -c /etc/postfix start
 
-tail -f /var/log/mail.log | iconv --to-code=UTF8
+tail -f /var/log/mail.log
 

9244a150bac3d8d73868b0b16d16944f28600863
Rafa Couto <caligari@treboada.net>
2016-12-29 05:13:54 +0100

    List of delivered domains.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ postconf -e myhostname="$HOSTNAME.$DOMAIN"
 postconf -e mydomain="$DOMAIN"
 postconf -e smtpd_banner="\$myhostname ESMTP"
 postconf -e mail_spool_directory="/var/spool/mail/"
+postconf -e mydestination="localhost, $myhostname, $mydomain"
 postconf -Me submission/inet="submission inet n - - - - smtpd"
 
 postconf 
